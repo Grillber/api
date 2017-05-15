d3e02ddb07a6d1d159e33df94daa0f28ce4a39ad
drSquabs <mike.p.dagenais@gmail.com>
2017-05-15 16:45:39 -0400

    Added main foundation for Grillber API

--- index.js
+++ index.js
@@ -1,25 +1,30 @@
-
-//server.js
+//Server
 
 // Packages/Dependencies 
 const express     = require('express');     //call express
 const app         = express();              //Define app using express
+const mysql       = require('promise-mysql');
 const bodyParser  = require('body-parser'); 
+
+// Data loader
+const DashboardlyDataLoader = require('./lib/grillber_api.js');
+
+// Controllers
+const authController = require('./controllers/auth.js');
+const boardsController = require('./controllers/bookings.js');
+const bookmarksController = require('./controllers/products.js');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 var port = process.env.PORT || 1337; 
-//DB connection 
-// var knex = require('knex')({
-//   client: 'mysql',
-//   connection: {
-//     host : 'localhost',
-//     user : 'root',
-//     password : '',
-//     database : 'myapp_test'
-//   }
-// });
+
+// Database / data loader initialization
+const connection = mysql.createPool({
+  user: 'root',
+  database: 'grillber'
+});
+const dataLoader = new DashboardlyDataLoader(connection);
 
 // Routes for API
 var router = express.Router();
@@ -33,7 +38,15 @@ router.get('/', function(req, res) {
 
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(checkLoginToken(dataLoader));
+app.use(cors());
 app.use('/api', router);
+
+app.use('/auth', authController(dataLoader));
+app.use('/bookings', bookingsController(dataLoader));
+app.use('/products', productsController(dataLoader));
 
 // Server start
 app.listen(port);
