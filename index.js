29140bf73c1b97fdf30eb58ff338cfc91375b448
drSquabs <mike.p.dagenais@gmail.com>
2017-05-16 12:02:05 -0400

    Added cors and router support

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const checkLoginToken = require('./lib/check-login-token.js');
 const cors = require('cors');
+const router = require('router');
 
 // Data loader
 const GrillberDataLoader = require('./lib/grillber_api.js');
