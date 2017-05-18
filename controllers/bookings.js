2f6555885e44406f5fb0aae8b92bad94e47b9c9a
drSquabs <mike.p.dagenais@gmail.com>
2017-05-17 20:02:02 -0400

    Bookings endpoints to be debugged

--- controllers/bookings.js
+++ controllers/bookings.js
@@ -7,19 +7,19 @@ module.exports = (dataLoader) => {
 
 //Create Order
   bookingsController.post('/', onlyLoggedIn, (req, res) => {
-    console.log(req.body.location);
     dataLoader.createBookings({
       dropDate: req.body.dropDate,
       pickUpDate: req.body.pickUpDate,
-      sum: req.body.orderTotal,
-      location: req.body.location
+      bookingTotal: req.body.bookingTotal,
+      location: req.body.location,
+      status: req.body.status
     })
      .then(data => res.json(data))
      .catch(err => res.status(400).json(err));
   });
   
   //Retrieve previous orders
-  bookingsController.get('/', (req, res) => {
+  bookingsController.get('/', onlyLoggedIn, (req, res) => {
     dataLoader.getAllBookings({
       page: req.query.page,
       limit: req.query.count
